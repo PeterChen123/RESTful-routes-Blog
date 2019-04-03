bc8a1fe8d0a7c35f9a380592c32f2deef77a71b4
PeterChen123 <chenguanyu9745@outlook.com>
2019-04-02 19:36:33 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var express     = require("express"),
+    methodOverride = require("method-override"),
+    expressSanitizer = require("express-sanitizer"),
     app         = express(),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose");
@@ -8,6 +10,8 @@ mongoose.connect("mongodb://localhost:27017/restful_blog_app", {useNewUrlParser:
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
+app.use(methodOverride("_method"));
 
 // MONGOOSE/MODEL Config
 var blogSchema = new mongoose.Schema({
@@ -24,6 +28,7 @@ app.get("/", function(req, res) {
     res.redirect("/blogs");
 });
 
+// INDEX ROUTE
 app.get("/blogs", function(req, res){
     Blog.find({}, function(err, blogs){
         if(err){
@@ -35,6 +40,75 @@ app.get("/blogs", function(req, res){
     });
 });
 
+// NEW ROUTE
+app.get("/blogs/new", function(req, res) {
+    res.render("new");
+});
+
+// CREATE ROUTE
+app.post("/blogs", function(req, res){
+    // create blog
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    Blog.create(req.body.blog, function(err, newBlog){
+        if(err){
+            res.render("new");
+        }
+        else{
+            // then,redirect to the index
+            res.redirect("/blogs");
+        }
+    });
+});
+
+// SHOW ROUTE
+app.get("/blogs/:id", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        }
+        else{
+            res.render("show", {blog: foundBlog});
+        }
+    });
+});
+
+// EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        }
+        else{
+            res.render("edit", {blog: foundBlog});
+        }
+    });
+});
+
+// UPDATE ROUTE
+app.put("/blogs/:id", function(req, res) {
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+        if(err){
+            res.redirect("/blogs");
+        }
+        else{
+            res.redirect("/blogs/" + req.params.id);
+        }
+    });
+});
+
+// DESTROY ROUTE
+app.delete("/blogs/:id", function(req, res) {
+    // destroy blog
+    Blog.findByIdAndRemove(req.params.id, function(err) {
+        if(err) {
+            res.redirect("/blogs");
+        }
+        
+        else {
+            res.redirect("/blogs");
+        }
+    });
+});
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
